7b6e79544831c4f0caa9deb02b213a596c45805d
Sanil Khurana <sanilkhurana7@gmail.com>
2020-06-25 03:27:07 +0530

    Completed the first assignment

--- week1/assignment-1/index.js
+++ week1/assignment-1/index.js
@@ -2,31 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dish-router');
+const leaderRouter = require('./routes/leader-router');
+const promoRouter = require('./routes/promo-router');
 
 app = express();
 
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
-
-
-app.get('/dish/:dishId', (req, res) => {
-    res.end('Will send all the dishes dish details of dishId '+req.params.dishId);
-});
-
-app.post('/dish/:dishId', (req, res, next) => {
-    res.status = 405;
-    res.end('POST operation not supported');
-});
-
-app.put('/dish/:dishId', (req, res, next) => {
-    res.end('Will update the dish with dishId '+req.params.dishId);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish here '+req.params['dishId']);
-});
-
+app.use('/leaders', leaderRouter);
+app.use('/promotions', promoRouter);
 
 PORT = 3000;
 app.listen(PORT, () => {
