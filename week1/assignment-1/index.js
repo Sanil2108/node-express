055f7a3840b59196b1d93b00131ef1517f979776
Sanil Khurana <sanilkhurana7@gmail.com>
2020-06-25 03:05:02 +0530

    Refactored express router

--- week1/assignment-1/index.js
+++ week1/assignment-1/index.js
@@ -1,39 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dish-router');
+
 app = express();
 
 app.use(bodyParser.json());
 
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-
-    next();
-});
-
-app.get('/dishes', (req, res) => {
-    res.end('Will send all the dishes to you');
-});
-
-app.post('/dishes', (req, res, next) => {
-    res.end(`Will add the dish: ${req.body.name} with details ${req.body.description}`);
-});
-
-app.put('/dishes', (req, res, next) => {
-    res.status = 405;
-    res.end('PUT operation not supported');
-});
-
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all the dishes here');
-});
-
-
+app.use('/dishes', dishRouter);
 
 
 app.get('/dish/:dishId', (req, res) => {
-    res.end('Will send all the dishes dish details of disId '+req.params.dishId);
+    res.end('Will send all the dishes dish details of dishId '+req.params.dishId);
 });
 
 app.post('/dish/:dishId', (req, res, next) => {
@@ -49,14 +27,6 @@ app.delete('/dishes/:dishId', (req, res, next) => {
     res.end('Deleting dish here '+req.params['dishId']);
 });
 
-
-
-
-
-// router.post('/', (req, res) => {
-//     console.log(req.body)
-//     res.send('Hello world');
-// })
 
 PORT = 3000;
 app.listen(PORT, () => {
--- week1/express/index.js
+++ week1/express/index.js
@@ -2,39 +2,11 @@ const express = require('express');
 const http = require('http');
 
 const PORT = 3000;
-const HOST = 'localhost';
 
 const app = express();
-// next is an optional parameter that is used when working with middleware
-// What is use? https://stackoverflow.com/questions/11321635/nodejs-express-what-is-app-use
-app.use((req, res, next) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/html');
-
-    // Call the next middleware
-    next();
-})
-
-app.get('/', (req, res) => {
-    res.status = 200;
-    res.send('<h1>Hello, world</h1>');
-})
-
-
-app.get('/something:id', (req, res, next) => {
-    console.log('Function 1');
-    // res.send(`${parseInt(req.params['id']) + 1}`)
-    next('/');
-})
-
-// app.get('/something', (req, res, next) => {
-//     console.log('Function 2');
-//     res.send('Something2')
-//     next();
-// });
+const router = express.Router();
 
 
 app.listen(PORT, () => {
-    console.log(`App listening on ${PORT}`);
-});
+    console.log(`Running app on ${PORT}`);
+})
